c0490c9e5323b591646d524d2c6abd56ed4a6fcf
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-02-17 17:30:07 -0800

    better validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -10,12 +10,31 @@ class Transaction < ActiveRecord::Base
 
   validates :unit_price, :transaction_type, :quantity, presence: true
   validates :stock, :user, presence: true
-  validate :user_can_afford
+  validate :user_can_afford, :has_sufficient_shares
+
+
+  def buy?
+    transaction_type == 1
+  end
+
+  def sell?
+    transaction_type == -1
+  end
 
   private
   def user_can_afford
-    if transaction_type == :buy and unit_price * quantity > user.balance
+    if buy? and unit_price * quantity > user.balance
       errors.add(:quantity, "costs more than account contains")
+    end
+  end
+
+  private
+  def has_sufficient_shares
+    if sell?
+      shares_owned = Transaction.where(
+        user_id: self.user_id, stock_id: self.stock_id).map {|t| 
+        t.quantity * t.transaction_type }.inject(&:+)
+      errors.add(:quantity, "exceeds shares owned") if quantity > shares_owned
     end
   end
 
