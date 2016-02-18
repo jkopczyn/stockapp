45b2fd5229d9c1fe513771695a64e5eb07670ba9
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-02-17 16:22:23 -0800

    button disable tweak

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,11 +1,13 @@
 $(document).ready(function($) {
   $(".search-form").submit(function(event) {
     event.preventDefault();
+    $(event.target).find("button").prop("disabled", true);
     $.ajax({url: "/stocks/symbol/"+$(event.target).find("input").val(),
       datatype: "html",
       success: function(response) {
         $(".search-results").prepend(response);
         $(event.target).find("input").val("Search Again");
+        $(event.target).find("button").removeProp("disabled");
       }});
   });
 });
